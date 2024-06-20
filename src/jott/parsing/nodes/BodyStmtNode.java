94f135c3d07e9b7c9fe1667e1462959655e1fc57
Kat Delaney <ksd8518@rit.edu>
2024-06-19 23:38:55 -0400

    whileloopnode maybe done?

--- src/jott/parsing/nodes/BodyStmtNode.java
+++ src/jott/parsing/nodes/BodyStmtNode.java
@@ -1,6 +1,15 @@
 package jott.parsing.nodes;
 
 import jott.JottTree;
+import jott.parsing.ParseContext;
 
 public class BodyStmtNode extends JottNode {
+    public static BodyStmtNode parse(ParseContext ctx) {
+        return null; // for now
+    }
+
+    @Override
+    public String convertToJott() {
+        return ""; // for now
+    }
 }
--- src/jott/parsing/nodes/BoolNode.java
+++ src/jott/parsing/nodes/BoolNode.java
@@ -1,4 +1,14 @@
 package jott.parsing.nodes;
 
-public class BoolNode {
+import jott.parsing.ParseContext;
+
+public class BoolNode extends JottNode {
+    public static BoolNode parse(ParseContext ctx){
+        return null; // for now
+    }
+
+    @Override
+    public String convertToJott() {
+        return ""; // for now
+    }
 }
