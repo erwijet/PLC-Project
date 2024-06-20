4eb8364bc6b389bba14cae70236d719aafc008eb
Kat Delaney <ksd8518@rit.edu>
2024-06-20 14:39:18 -0400

    assignment done

--- src/jott/parsing/nodes/AssignmentNode.java
+++ src/jott/parsing/nodes/AssignmentNode.java
@@ -2,15 +2,24 @@ package jott.parsing.nodes;
 
 
 import jott.parsing.ParseContext;
+import jott.tokenization.Token;
+import jott.tokenization.TokenType;
 
 public class AssignmentNode extends JottNode {
+    private Token id;
+    private ExprNode exp;
 
     static AssignmentNode parse(ParseContext ctx) {
-
+        AssignmentNode node = new AssignmentNode();
+        node.id = ctx.eat(TokenType.ID_KEYWORD);
+        ctx.eat(TokenType.ASSIGN);
+        node.exp = ExprNode.parse(ctx);
+        ctx.eat(TokenType.SEMICOLON);
+        return node;
     }
 
     @Override
     public String convertToJott() {
-        return null;
+        return id.getToken() + " = " + exp.convertToJott() + ";";
     }
 }
