eca91ece98bf85e25a6a2255ff78b3df7c89088a
Kat Delaney <ksd8518@rit.edu>
2024-06-20 00:10:37 -0400

    else, elseif, and if done

--- src/jott/parsing/nodes/ElseNode.java
+++ src/jott/parsing/nodes/ElseNode.java
@@ -1,4 +1,22 @@
 package jott.parsing.nodes;
 
-public class ElseNode {
+import jott.parsing.ParseContext;
+import jott.tokenization.TokenType;
+
+public class ElseNode extends JottNode {
+    private BodyStmtNode body;
+
+    public static ElseNode parse(ParseContext ctx){
+        ElseNode node = new ElseNode(); // not actually sure if making a node is needed?
+        ctx.eat(TokenType.ID_KEYWORD, "Else");
+        ctx.eat(TokenType.L_BRACE);
+        node.body = BodyStmtNode.parse(ctx);
+        ctx.eat(TokenType.R_BRACKET);
+        return node;
+    }
+
+    @Override
+    public String convertToJott() {
+        return "Else{\n" + body.convertToJott() + "\n}";
+    }
 }
--- src/jott/parsing/nodes/IfStmtNode.java
+++ src/jott/parsing/nodes/IfStmtNode.java
@@ -1,4 +1,26 @@
 package jott.parsing.nodes;
 
-public class IfStmtNode {
+import jott.parsing.ParseContext;
+import jott.tokenization.TokenType;
+
+public class IfStmtNode extends JottNode {
+    private BoolNode condition;
+    private BodyStmtNode body;
+
+    public static IfStmtNode parse(ParseContext ctx){
+        IfStmtNode node = new IfStmtNode(); // not actually sure if making a node is needed?
+        ctx.eat(TokenType.ID_KEYWORD, "If");
+        ctx.eat(TokenType.L_BRACKET);
+        node.condition = BoolNode.parse(ctx);
+        ctx.eat(TokenType.R_BRACKET);
+        ctx.eat(TokenType.L_BRACE);
+        node.body = BodyStmtNode.parse(ctx);
+        ctx.eat(TokenType.R_BRACKET);
+        return node;
+    }
+
+    @Override
+    public String convertToJott() {
+        return "If[" + condition.convertToJott() + "]{\n" + body.convertToJott() + "\n}";
+    }
 }
