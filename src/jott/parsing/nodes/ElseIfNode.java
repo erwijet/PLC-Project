95e1f0b193f399a689807e51fa0732526c693f62
Kat Delaney <ksd8518@rit.edu>
2024-06-20 14:31:28 -0400

    minor fix as conditions are expressions

--- src/jott/parsing/nodes/ElseIfNode.java
+++ src/jott/parsing/nodes/ElseIfNode.java
@@ -4,14 +4,14 @@ import jott.parsing.ParseContext;
 import jott.tokenization.TokenType;
 
 public class ElseIfNode extends JottNode {
-    private BoolNode condition;
+    private ExprNode condition;
     private BodyStmtNode body;
 
     public static ElseIfNode parse(ParseContext ctx){
         ElseIfNode node = new ElseIfNode(); // not actually sure if making a node is needed?
         ctx.eat(TokenType.ID_KEYWORD, "Elseif");
         ctx.eat(TokenType.L_BRACKET);
-        node.condition = BoolNode.parse(ctx);
+        node.condition = ExprNode.parse(ctx);
         ctx.eat(TokenType.R_BRACKET);
         ctx.eat(TokenType.L_BRACE);
         node.body = BodyStmtNode.parse(ctx);
