ed9f220e8156a2d7f7e4fa7a924219fabec995c3
Kat Delaney <ksd8518@rit.edu>
2024-07-03 21:07:23 -0400

    jott main class made, need to figure out how to validate tree

---
+++ src/jott/Jott.java
@@ -0,0 +1,53 @@
+package jott;
+
+import jott.tokenization.JottTokenizer;
+import jott.tokenization.Token;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Jott {
+    public static void main(String[] args) {
+        if(args.length != 3) { // incorrect number of args
+            System.out.println("Usage: java Jott <input_file> <output_file> <output_language>");
+            return;
+        }
+        String input = args[0];
+        String output = args[1];
+        String language = args[2];
+        try{
+            File in = new File(input);
+            if(!in.exists() || !in.isFile() || !in.canRead()) { // makes sure input file is legit
+                System.out.println("Error: input file " + input + " does not exist, is not a file, or is not readable.");
+                return;
+            }
+            ArrayList<Token> tokens = JottTokenizer.tokenize(input);
+            JottTree tree = JottParser.parse(tokens);
+            assert tree != null;
+            tree.validateTree();
+            String code = "";
+            if(language.equalsIgnoreCase("jott"))
+                code += tree.convertToJott();
+            else if(language.equalsIgnoreCase("java")) // todo: get proper class name
+                code += tree.convertToJava("");
+            else if(language.equalsIgnoreCase("python"))
+                code += tree.convertToPython();
+            else if(language.equalsIgnoreCase("c"))
+                code += tree.convertToC();
+            else { // invalid language
+                System.out.println("Error: Language " + language + " is not supported. Please use 'Jott', 'Java', 'Python', or 'C'");
+                return;
+            }
+            BufferedWriter writer = new BufferedWriter(new FileWriter(output));
+            writer.write(code);
+            writer.close();
+        }
+        catch (IOException e){
+            System.out.println("Error: output file " + output + "is not a valid file or is not readable/writeable.");
+            return;
+        }
+    }
+}
