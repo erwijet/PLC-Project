9688be12964b37d0962a2779b26aed6205b3e6d7
Kat Delaney <ksd8518@rit.edu>
2024-07-11 18:28:29 -0400

    exceptions

--- src/jott/SemanticException.java
+++ src/jott/SemanticException.java
@@ -11,22 +11,36 @@ public class SemanticException extends RuntimeException {
         MISSING_RETURN_TYPE,
         UNKNOWN_FUNCTION,
         UNKNOWN_BINDING,
+        MISSING_MAIN,
         MALFORMED_TREE
     }
 
     private static String buildMessage(Cause cause, Token token, Object expected, Object found) {
         return switch (cause) {
-            case TYPE_CONFLICT -> String.format("Semantic Error\nInvalid type for '%s'. Expected %s, but found %s\n%s",
+            case TYPE_CONFLICT -> String.format("Semantic Error:\nInvalid type for '%s'. Expected %s, but found %s\n%s:%d",
                     token.getToken(),
                     expected.toString(),
                     found.toString(),
-                    token);
-            case MISSING_RETURN_TYPE -> null;
-            case UNKNOWN_FUNCTION -> null;
-            case UNKNOWN_BINDING -> null;
+                    token.getFilename(),
+                    token.getLineNum());
+            case MISSING_RETURN_TYPE -> String.format("Semantic Error:\nMissing return for non-Void function %s\n%s:%d",
+                    token.getToken(),
+                    token.getFilename(),
+                    token.getLineNum());
+            case UNKNOWN_FUNCTION -> String.format("Semantic Error:\nCall to unknown function '%s'\n%s:%d",
+                    token.getToken(),
+                    token.getFilename(),
+                    token.getLineNum());
+            case UNKNOWN_BINDING -> String.format("Semantic Error:\nCall to unknown variable '%s'\n%s:%d",
+                    token.getToken(),
+                    token.getFilename(),
+                    token.getLineNum());
+            case MISSING_MAIN -> String.format("Semantic Error:\nMissing or incorrectly defined main function\n%s:%d",
+                    token.getFilename(),
+                    token.getLineNum());
             // this one really shouldn't ever happen. it's to handle if we expect a relop expr
             // to have 3 children (left, opt, right) and it has less or something
-            case MALFORMED_TREE -> null;
+            case MALFORMED_TREE -> String.format("Semantic Error:\nMalformed tree");
         };
     }
 
