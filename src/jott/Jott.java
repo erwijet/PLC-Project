9faa7437d86d0b9af5ca1f51a1cb8fb1fc70997b
Kat Delaney <ksd8518@rit.edu>
2024-07-11 19:09:47 -0400

    edit main function slightly

--- src/jott/Jott.java
+++ src/jott/Jott.java
@@ -1,5 +1,7 @@
 package jott;
 
+import jott.parsing.ParseException;
+import jott.tokenization.JottTokenizationException;
 import jott.tokenization.JottTokenizer;
 import jott.tokenization.Token;
 
@@ -17,15 +19,19 @@ public class Jott {
         }
         String input = args[0];
         String output = args[1];
-        String language = args[2];
+        String language;
+        if (!((language = args[2].toLowerCase()).equals("jott") || language.equals("java") || language.equals("python") || language.equals("c"))) { // unsupported language
+            System.out.println("Error: <output_language> must be either 'jott', 'java', 'python', or 'c'. " + language + " is not supported");
+            return;
+        }
         try{
             File in = new File(input);
             if(!in.exists() || !in.isFile() || !in.canRead()) { // makes sure input file is legit
                 System.out.println("Error: input file " + input + " does not exist, is not a file, or is not readable.");
                 return;
             }
-            ArrayList<Token> tokens = JottTokenizer.tokenize(input); // todo: may need try catch here
-            JottTree tree = JottParser.parse(tokens); // todo: prob need try catch here
+            ArrayList<Token> tokens = JottTokenizer.tokenize(input);
+            JottTree tree = JottParser.parse(tokens);
             assert tree != null;
             if(tree.validateTree()) { // todo: needs to run entire tree which it isnt set up for currently
                 String code = "";
@@ -35,22 +41,17 @@ public class Jott {
                     code += tree.convertToJava("");
                 else if (language.equalsIgnoreCase("python"))
                     code += tree.convertToPython();
-                else if (language.equalsIgnoreCase("c"))
+                else
                     code += tree.convertToC();
-                else { // invalid language
-                    System.out.println("Error: Language " + language + " is not supported. Please use 'Jott', 'Java', 'Python', or 'C'");
-                    return;
-                }
                 BufferedWriter writer = new BufferedWriter(new FileWriter(output));
                 writer.write(code);
                 writer.close();
             }
-            else
-                System.out.println("Invalid tree");
         }
-        catch (IOException e){
+        catch (IOException e){ // not valid output file
             System.out.println("Error: output file " + output + "is not a valid file or is not readable/writeable.");
-            return;
+        }
+        catch(ParseException | JottTokenizationException | SemanticException e){ // tokenizer, parser, semantic exception
         }
     }
 }
