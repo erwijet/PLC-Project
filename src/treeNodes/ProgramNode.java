f34edd2beb29d1a845d056e2f6fbb5ebe0d63969
Tyler Holewinski <tyler.holewinski@bryx.com>
2024-08-02 23:28:55 -0400

    fix: polyfill for dayz

--- src/treeNodes/ProgramNode.java
+++ src/treeNodes/ProgramNode.java
@@ -49,10 +49,18 @@ public class ProgramNode extends JottNode {
                 #include <string.h>
                 #include <stdlib.h>
                 
-                char* __jott_concat(char* s1, char* s2) {
-                  strcat(s1, s2);
-                  return s1;
+                char* __jott_concat(char* s1, const char* s2) {
+                    size_t len1 = strlen(s1);
+                    size_t len2 = strlen(s2);
+                                
+                    char* result = (char*)malloc(len1 + len2 + 1);
+                                
+                    strcpy(result, s1);
+                    strcat(result, s2);
+                                
+                    return result;
                 }
+                                    
                 """);
         for (FuncDefNode function : functions)
             ret.append(function.convertToC()).append("\n");
